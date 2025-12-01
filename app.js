d8a7998e5fe0508c9a281282354d93a31e42e41f
Ruthless Calm <ruthlesscalm@proton.me>
2025-12-01 21:02:42 +0530

    feat: add login route and create login page with form for user authentication

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"views/index.html"));  //change the location later after using ejs
 })
 
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname,"views/login.html"));
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
